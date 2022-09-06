996ea2961e95458a5ae406bcff096c0f1648e72a
unknown <paingminnoo>
2022-09-06 12:15:47 +0630

    same to same check api testing

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(express.json());
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "xzonebet"
+    database: "xzonebet",
 });
 
 con.connect(function(err) {
     if (err) throw err;
-    con.query("SELECT * FROM configuration_settings", function(err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-    });
+    console.log("MySql Connected!");
 });
 
+app.get("/api/check-versions", (req, res) => {
+    con.query(
+        "SELECT android_version,ios_version FROM configuration_settings where id = 1",
+        function(err, result, fields) {
+            if (err) throw err;
+            res.send(result[0]);
+        }
+    );
+});
+
+//configuration_settings
 
 app.listen(3000, console.log(`Server is running on port 3000`));
